a865b1facaa09b12c26268a95547ca83308195b0
Tobias Riemenschneider <tobias.riemenschneider@climatepartner.com>
2023-01-02 11:55:20 +0100

    nest g module blog

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blog-project', {
       useNewUrlParser: true,
     }),
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
