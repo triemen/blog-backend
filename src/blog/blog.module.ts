1313061d12e010b4a1aebf8e00e24beef5f82c89
Tobias Riemenschneider <tobias.riemenschneider@climatepartner.com>
2023-01-02 12:14:52 +0100

    create service and connect with db

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogService } from './blog.service';
-
+import { BlogSchema } from './schemas/blog.schema';
 @Module({
-  providers: [BlogService]
+  imports: [MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }])],
+  providers: [BlogService],
 })
 export class BlogModule {}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreatePostDTO } from './dto/create-post.dto';
+import { Post } from './interfaces/post.interface';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {}
+
+  async addPost(createPostDTO: CreatePostDTO): Promise<Post> {
+    const newPost = new this.postModel(createPostDTO);
+    return newPost.save();
+  }
+
+  async getPost(postID): Promise<Post> {
+    const post = await this.postModel.findById(postID).exec();
+    return post;
+  }
+
+  async getPosts(): Promise<Post[]> {
+    const posts = await this.postModel.find().exec();
+    return posts;
+  }
+
+  async editPost(postID, createPostDTO: CreatePostDTO): Promise<Post> {
+    const editedPost = await this.postModel.findByIdAndUpdate(
+      postID,
+      createPostDTO,
+      { new: true },
+    );
+    return editedPost;
+  }
+
+  async deletePost(postID): Promise<any> {
+    const deletedPost = await this.postModel.findByIdAndRemove(postID);
+    return deletedPost;
+  }
+}
